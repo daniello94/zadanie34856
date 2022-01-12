44cab1bbeca25c550ec3133e8291bda753909790
Daniel Domalik <faktury12d@gmail.com>
2022-01-12 15:05:59 +0100

    zadanie

---
+++ jss/main.js
@@ -0,0 +1,7 @@
+let firstNumber = 8;
+let secondNumber = 10;
+function suma(firstNumber,secondNumber) {
+    let rezult = firstNumber + secondNumber;
+    return rezult ;
+}
+console.log(suma(firstNumber,secondNumber))
